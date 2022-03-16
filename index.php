e0db8085d6fca87b9db5ee91808b7fe94d496860
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-03-16 16:51:23 +0100

    navbar responsive

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV Barbie</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
-    <header></header>
+    <header>
+        <nav>
+            <h1>Barbie</h1>
+            <div class="items" id="items">
+                <a href="#">Mes formations</a>
+                <a href="#">Mes expériences</a>
+                <a href="#">Contact</a>
+            </div>
+
+            <a class="burger" href="#items">
+              <svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>  
+            </a>
+            
+        </nav>
+    </header>
     <main></main>
     <footer></footer>
 </body>
+
 </html>
