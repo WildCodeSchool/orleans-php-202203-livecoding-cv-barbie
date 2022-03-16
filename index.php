0f4f1b2c6f28a9994582993a3789f5a20caee29a
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-03-16 17:09:23 +0100

    fix nav

--- index.php
+++ index.php
@@ -14,20 +14,33 @@
         <nav>
             <h1>Barbie</h1>
             <div class="items" id="items">
-                <a href="#">Mes formations</a>
-                <a href="#">Mes expériences</a>
-                <a href="#">Contact</a>
+                <a href="#trainings">Mes formations</a>
+                <a href="#xp">Mes expériences</a>
+                <a href="#contact">Contact</a>
             </div>
 
             <a class="burger" href="#items">
-              <svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                </svg>  
+                <svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
             </a>
-            
+            <a class="close" href="#"></a>
         </nav>
     </header>
-    <main></main>
+    <main>
+        <section id="trainings">
+            <h1>Mes formations</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, iure? Dolorum ipsam sit labore quos voluptatum minus, libero laboriosam ipsum repellat! Aliquam possimus ipsa consequatur placeat cupiditate nesciunt veniam non.</p>
+        </section>
+        <section id="xp">
+            <h1>Mes expériences</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, iure? Dolorum ipsam sit labore quos voluptatum minus, libero laboriosam ipsum repellat! Aliquam possimus ipsa consequatur placeat cupiditate nesciunt veniam non.</p>
+        </section>
+        <section id="contact">
+            <h1>Contact</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, iure? Dolorum ipsam sit labore quos voluptatum minus, libero laboriosam ipsum repellat! Aliquam possimus ipsa consequatur placeat cupiditate nesciunt veniam non.</p>
+        </section>
+    </main>
     <footer></footer>
 </body>
 
