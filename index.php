30d05ca018755c31c1c074fcce43552a56ef1a23
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-03-18 16:55:13 +0100

    trainings and PHP

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    require 'trainings.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -20,7 +23,7 @@
             </div>
 
             <a class="burger" href="#items">
-                <svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </a>
@@ -30,7 +33,15 @@
     <main>
         <section id="trainings">
             <h1>Mes formations</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, iure? Dolorum ipsam sit labore quos voluptatum minus, libero laboriosam ipsum repellat! Aliquam possimus ipsa consequatur placeat cupiditate nesciunt veniam non.</p>
+            <div class="trainings">
+                <?php foreach($trainings as $training) { ?>
+                    <div class="training">
+                        <h2><?php echo $training['school']; ?></h2>
+                        <div><?php echo $training['date']; ?></div>
+                        <p><?php echo $training['description']; ?></p>
+                    </div>
+                <?php } ?>
+            </div>
         </section>
         <section id="xp">
             <h1>Mes expériences</h1>
---
+++ trainings.php
@@ -0,0 +1,28 @@
+<?php
+$trainings = [
+    [
+        'school' => 'Collège de Barbieland',
+        'date' => '2015-2017',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam at quam velit iste, magni, possimus tempora explicabo aliquid est voluptas omnis consequatur voluptatum, ducimus tempore aspernatur odio in. Ullam.'
+    ],
+    [
+        'school' => 'Lycée de Barbieland',
+        'date' => '2017-2019',
+        'description' => 'Natus laboriosam at quam velit iste, magni, possimus tempora explicabo aliquid est voluptas omnis consequatur voluptatum, ducimus tempore aspernatur odio in. Ullam.'
+    ], 
+    [
+        'school' => 'Université de Barbieland',
+        'date' => '2020-2021',
+        'description' => 'Velit iste, magni, possimus tempora explicabo aliquid est voluptas omnis consequatur voluptatum, ducimus tempore aspernatur odio in. Ullam.'
+    ], 
+    [
+        'school' => 'Wild Code School',
+        'date' => '2022',
+        'description' => 'Mon formateur était super.'
+    ],  
+    [
+        'school' => 'Une autre formation',
+        'date' => '2023',
+        'description' => 'Mon formateur était moins bon.'
+    ],
+];
